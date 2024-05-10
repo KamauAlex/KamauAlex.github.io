b338ac24966c35c6b20a14190b57c81065c76544
KamauN <alexander.njuguna@around25.com>
2024-05-11 00:48:18 +0300

    Add configurations

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
+	output: 'export',
+	reactStrictMode: false,
+	swcMinify: true,
 }
 
 module.exports = nextConfig
